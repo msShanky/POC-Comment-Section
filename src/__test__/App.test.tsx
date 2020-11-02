4e54693bf457de6fcfc927316b755f9968301571
Shankara Narayanan <shankaranarayanan.m.s@gmail.com>
2020-11-02 16:09:43 +0530

    Initial commit

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../App";
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("should be equal to 4", () => {
+  expect(3).toEqual(3);
+});
